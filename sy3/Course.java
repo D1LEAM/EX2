10ea166ef4c43876b9b9e4c7d655d179f2a86494
王逸飞 <72386659+D1LEAM@users.noreply.github.com>
2020-10-19 20:18:56 +0800

    Update Course.java

--- sy3/Course.java
+++ sy3/Course.java
@@ -6,7 +6,6 @@ public class Course {
 	 private Teacher teacher;
 	 private float credit;
 	 private Student[] students;
-	 //构造函数
 	 public Course(int courseId,String courseName,float credit,Teacher teacher) {
 	  super();
 	  this.courseId=courseId;
@@ -30,12 +29,11 @@ public class Course {
 	  students = new Student[30];
 	 }
 	 
-	 public Course() {//默认形式，要有以防万一
+	 public Course() {
 	  super();
 	  students = new Student[30];
 	 }
 	 
-	 //修改或获取属性值id，name，credit，
 	 public void setId(int id){
 	  this.courseId=id;
 	 }
@@ -61,10 +59,8 @@ public class Course {
 	  this.teacher = teacher;
 	 }
 	 
-	 //课加入学生
 	 public boolean addStudent(Student stu){
-	  boolean flag = false;//标志值：是否加入成功
-	  //如果学生没有选过这门课，同时课的学生还没满则执行
+	  boolean flag = false;
 	  if(!isSelectedStudent(stu)&&isNullStudent(stu)){
 	   for(int i=0;i<students.length;i++){
 	    if(students[i]==null){
@@ -76,10 +72,9 @@ public class Course {
 	  }
 	  return flag;
 	 }
-	 //课移除学生
 	 public boolean removeStudent(Student stu){
 	  boolean flag=false;
-	  if(isSelectedStudent(stu)){//选过这门课
+	  if(isSelectedStudent(stu)){
 	   for(int i=0;i<students.length;i++){
 	    if(students[i]==stu){
 	     students[i]=null;
@@ -90,9 +85,8 @@ public class Course {
 	  }
 	  return flag;
 	 }
-	 //显示选择课程的学生：
 	 public void displayStudent(){
-	  System.out.println("选择的课程："+this.courseName+"的学生有:");
+	  System.out.println("閫夋嫨鐨勮绋嬶細"+this.courseName+"鐨勫鐢熸湁:");
 	  for(Student s:students){
 	   if(s!=null){
 	    System.out.print(s.getStuName()+" ");
@@ -100,10 +94,9 @@ public class Course {
 	  }
 	  System.out.println();
 	 }
-	 //子方法1：学生是否选过这门课
-	 public boolean isSelectedStudent(Student stu){
+	  public boolean isSelectedStudent(Student stu){
 	  boolean flag=false;
-	  for(Student s:students){//只能用于检查，不能修改
+	  for(Student s:students){
 	   if(s==stu){
 	    flag=true;
 	    break;
@@ -111,11 +104,10 @@ public class Course {
 	  }
 	  return flag;
 	 }
-	 //子方法2：学科学生未达到限定人数吗
-	 public boolean isNullStudent(Student stu){
+	  public boolean isNullStudent(Student stu){
 	  boolean flag=false;
 	  for(Student s:students){
-	   if(s==null){//还有空位
+	   if(s==null){
 	    flag=true;
 	    break;
 	   }
@@ -123,9 +115,5 @@ public class Course {
 	  return flag;
 	 }
 	 public static void main(String[] args) {
-	  // TODO Auto-generated method stub
-	 
-	 }
-	 
-	 
+	   }
 	}
